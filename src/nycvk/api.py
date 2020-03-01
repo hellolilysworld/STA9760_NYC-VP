0dc463d61f324f4818cab234f6ea0f5d79be144f
hellolilysworld <57305925+hellolilysworld@users.noreply.github.com>
2020-02-29 22:03:18 -0500

    Update api.py

--- src/nycvk/api.py
+++ src/nycvk/api.py
@@ -1,5 +1,3 @@
-#import pandas as pd
-
 from sodapy import Socrata
 
 def get_data(app_key,page_size,num_pages):
@@ -7,12 +5,11 @@ def get_data(app_key,page_size,num_pages):
 		
 	client = Socrata("data.cityofnewyork.us",app_key)
 
-	#pag = page_size * (num_pages - 1)
+	
 	results = []
 	for i in range(0, num_pages):
 		results.append(client.get('nc67-uf89', limit=page_size, offset=i*(page_size)))
 		
-	# results = client.get("nc67-uf89", limit=page_size, offset = pag) #get results.json
-	#results_df = pd.DataFrame.from_records(results)
+	
 	return results
-	# print(results)
+	
