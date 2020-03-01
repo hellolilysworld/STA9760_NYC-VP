d54921d0e68fd8a0e043ef3dcdda7f2864b6dfda
hellolilysworld <57305925+hellolilysworld@users.noreply.github.com>
2020-02-29 22:03:50 -0500

    Delete json.py

--- src/nycvk/json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def save_as_json(data,output):
-	#json_object = json.dumps(data, indent=4)
-	#json_object = json.dumps(data)
-	#json_object = to_json(data)
-	with open(output, "w") as outfile: 
-		outfile.write(f"{data}")
-
